5925773d7b0e6743a0bc4498315af7fbe15999bc
am2t <135924202+am2t@users.noreply.github.com>
2023-06-07 18:37:08 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.showString("Ejercicios")
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        . # . # .
+        `)
+})
